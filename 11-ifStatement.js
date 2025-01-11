736aee8dc6edf7dc893d8cdb0036d938b690f4a5
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-12 00:32:06 +0530

    if statement learnt and file added

---
+++ 11-ifStatement.js
@@ -0,0 +1,19 @@
+console.log("@@@@@@@@@@@ If Statement @@@@@@@@@@@");
+
+let age = 34;
+let mode = "dark";
+let color;
+
+if (mode == "dark") {
+    color = "black";
+}
+
+if (mode == "light") {
+    color = "white";
+}
+
+console.log(color);
+
+if (age >= 18) {
+    console.log("Yesss!!! you can vote now!!!");
+}

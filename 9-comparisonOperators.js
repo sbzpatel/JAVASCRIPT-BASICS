b2fe54c6d3b256d94c32a4774c5cdef7471ae862
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-11 23:57:03 +0530

    comparison operators learnt and file added

---
+++ 9-comparisonOperators.js
@@ -0,0 +1,30 @@
+console.log("@@@@@@@@@@@ Comparison Operators @@@@@@@@@@@");
+
+    // == (Equal to)
+    // === (Equal to & type)
+    // != (Not Equal to)
+    // !== (Not Equal to & type)
+    // < (less than)
+    // > (greater than)
+    // <= (less than & equal to)
+    // >= (greater than & equal to)
+
+
+    let x = 5;
+    let y = 6;
+
+    console.log("x == y", x == y);      // false
+    console.log("x != y", x != y);      // true
+
+
+    y = "5";
+
+    console.log("x === y", x === y);    // false
+    console.log("x !== y", x !== y);    // true
+
+    y = 7;
+
+    console.log("x < y", x < y)         // true
+    console.log("x > y", x > y)         // false
+
+console.log("@@@@@@@@@@@ Comparison Operators @@@@@@@@@@@");

1d65bd5ae1c90482bb096cc6bf91d72cdc17e0a0
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-11 23:52:44 +0530

    arithmetic operators learnt and file added

---
+++ 6-arithmeticOperators.js
@@ -0,0 +1,19 @@
+// Used to perform some operations on data
+
+// a+b is expression, where "a" & "b" are operands and "+"" is operator
+
+console.log("@@@@@@@@@@@ Arithmetic Operators @@@@@@@@@@@");
+
+    // Arithmetic Operator ->> +, -, *, /, %, ** 
+    
+    let a = 6;
+    let b = 2;
+
+    console.log("a + b = ", a + b);
+    console.log("a - b = ", a - b);
+    console.log("a * b = ", a * b);
+    console.log("a / b = ", a / b);
+    console.log("a % b = ", a % b);
+    console.log("a ** b = ", a ** b);       // 6^2 = 36 
+
+console.log("@@@@@@@@@@@ Arithmetic Operators @@@@@@@@@@@");

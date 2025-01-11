4d9582fe5e01413d223bb13b3735dcc4d7f4476b
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-11 23:55:52 +0530

    assignment operators learnt and file added

---
+++ 8-assignmentOperators.js
@@ -0,0 +1,13 @@
+console.log("@@@@@@@@@@@ Assignment Operators @@@@@@@@@@@");
+
+    // Assignment Operators =>> =, +=, -=, *=, /=
+
+    let a = 6;
+    let b = 2;
+
+    console.log("a += 4: ", a+=4);    // "a+=4" is equaivalent of "a=a+4" 
+    console.log("a -= 4: ", a-=4);    // "a-=4" is equaivalent of "a=a-4" 
+    console.log("a *= 4: ", a*=4);    // "a*=4" is equaivalent of "a=a*4" 
+    console.log("a /= 4: ", a/=4);    // "a/=4" is equaivalent of "a=a/4" 
+
+console.log("@@@@@@@@@@@ Assignment Operators @@@@@@@@@@@");

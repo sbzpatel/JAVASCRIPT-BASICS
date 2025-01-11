cc4866ebf33e08835ee20dab62fd1ceb5bea512a
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-11 22:52:50 +0530

    datatypes learnt and added

---
+++ 4-dataTypes.js
@@ -0,0 +1,31 @@
+console.log("@@@@@@@@@@@ Data Types @@@@@@@@@@@");
+
+// Javascript DataTypes (There are 2 types of Datatypes)
+// 1) Primitive DataTypes => (Number, String, Boolean, Undefined, Null, BigInt, Symbol) 
+// 2) Non-Primitive DataTypes (Arrays, Objects)
+
+let age = 24;
+console.log(typeof age);
+
+
+console.log(typeof "Sbz");
+
+
+let married = true;
+console.log(typeof married);
+
+
+let wifeName;
+console.log(typeof wifeName);
+
+
+let salary = null;
+console.log(typeof salary);     // null variable is treated as an "object"
+
+
+let x = BigInt("12345");
+console.log(typeof x);
+
+let y = Symbol("Sbz Patel");
+console.log(typeof y);
+

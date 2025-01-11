d01ed5b8bcca57d95e4ec8f7fdcd8637eb54e621
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-11 23:58:47 +0530

    logical operators learnt and file added

---
+++ 10-logicalOperators.js
@@ -0,0 +1,15 @@
+console.log("@@@@@@@@@@@ Logical Operators @@@@@@@@@@@");
+
+    // && (Logical AND), if both are true in that situation only condition is true, otherwise always false
+    // || (Logical OR), if any one condition is true than condition is true, if both false only in that condn false
+    // ! (Logical NOT), true converts false and vice versa
+
+    let x = 5;
+    let y = 6;
+
+    let cond1 = x === 5;        // true
+    let cond2 = y > x;          // true
+
+    console.log("cond1 && cond2 = ", cond1 && cond2);   // true
+
+console.log("@@@@@@@@@@@ Logical Operators @@@@@@@@@@@");

696688e7d0093dcf26e7c48fa08096fbc9577742
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-12 00:34:53 +0530

    if else statement learnt and file added

---
+++ 12-ifElseStatement.js
@@ -0,0 +1,18 @@
+console.log("@@@@@@@@@@@ If-else Statement @@@@@@@@@@@");
+
+let mode = "light";
+let color;
+
+let age = 34;
+
+if (mode == "light") {
+    color = "white";
+} else {
+    color = "black"
+}
+
+if (age >= 18) {
+    console.log("You can vote bro....");
+} else {
+    console.log("Sorry, You cannot vote!!!");
+}

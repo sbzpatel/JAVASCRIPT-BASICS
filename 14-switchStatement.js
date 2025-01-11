175694b7b0e0de478febe88882db6c61539f0633
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-12 00:42:27 +0530

    switch statement learnt and file added

---
+++ 14-switchStatement.js
@@ -0,0 +1,17 @@
+console.log("@@@@@@@@@@@ Switch Statement @@@@@@@@@@@");
+
+const language = "HTML";    // JAVA
+
+switch(language) {
+    case "HTML":
+        console.log("HTML selected.");
+    break;
+    case "CSS":
+        console.log("CSS selected.");
+    break;
+    default:
+        console.log("Invalid options!"); 
+    break;
+}
+
+

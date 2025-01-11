cbb20cc1186b0341f9a59a515f080457e3048d15
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-11 22:51:06 +0530

    variables file added

---
+++ 2-variables.js
@@ -0,0 +1,16 @@
+fullName = "Shahbaz Patel";
+age = 34;
+price = 101.56;
+x = null;
+y = undefined;
+isMarried = true;
+totalMarks = 555;
+$price = 405;
+
+// 123friend = "Kaleem";    // Wrong, variable can not starts with numeric
+friend1 = "Shahbaz";        // Right, variable can followed by numeric
+
+// fullName = 35;       // dynamic type where we can change the fullName with any type of value
+
+console.log(fullName);
+console.log($price);

424f8c441ea68c608290c911b925a5a900ccb4ef
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-11 23:54:25 +0530

    unary operators learnt and file added

---
+++ 7-UnaryOperators.js
@@ -0,0 +1,14 @@
+console.log("@@@@@@@@@@@ Unary Operators @@@@@@@@@@@");
+
+    // Unary Operatory ->> --, ++
+
+    let a = 6;
+    let b = 2;
+
+    a++;
+    console.log("a inc by 1 ->>", a);
+
+    b--;
+    console.log("b dec by 1 ->>", b);
+
+console.log("@@@@@@@@@@@ Unary Operators @@@@@@@@@@@");

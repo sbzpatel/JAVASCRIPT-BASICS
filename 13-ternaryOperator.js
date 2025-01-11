11987ee3d08adaf7b50510ebe5416f32206518f9
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-12 00:36:20 +0530

    ternary operator learnt and file added

---
+++ 13-ternaryOperator.js
@@ -0,0 +1,7 @@
+console.log("@@@@@@@@@@@ Ternary Operator @@@@@@@@@@@");
+
+let age = 34;
+
+let eligibleMsg = age >= 18 ? "Eligible" : "Not eligible";
+
+console.log("Your eligible status on age entered ->> ",eligibleMsg);

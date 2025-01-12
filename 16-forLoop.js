e522b654251c2e5fa3136c8a10c2b4d99829d778
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-12 11:58:28 +0530

    for loop learnt and file added

---
+++ 16-forLoop.js
@@ -0,0 +1,26 @@
+console.log("@@@@@@@@@@@ For Loop @@@@@@@@@@@");
+
+// console.log("Shahbaz Patel");
+// console.log("Shahbaz Patel");
+// console.log("Shahbaz Patel");
+// console.log("Shahbaz Patel");
+// console.log("Shahbaz Patel");
+
+// print string 100 times
+for(let i=1; i<=100; i++) {
+    console.log("Shahbaz Patel");
+}
+
+// calculate sum of 1 to 50
+sum = 0;
+for(let m=1; m<=50; m++) {
+    sum = sum+m;
+}
+
+console.log("Sum =>",sum);
+
+for (var n=1; n<=5; n++) {
+    console.log("n =",n);
+}
+
+console.log("value of n after looping: ",n);

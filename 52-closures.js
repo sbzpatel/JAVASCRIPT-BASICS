ce9b1ae0f00dc5b2c6e7fc2a1602a29769db87b7
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-05-18 17:04:24 +0530

    add closures and objectProtype

---
+++ 52-closures.js
@@ -0,0 +1,61 @@
+// A closure allows a function to access variables from an outer function even after that outer function has returned.
+
+function outerFunction() {
+    let outerVariable = "I am Shahbaz Patel!!";
+    
+    function innerFunction() {
+        console.log(outerVariable);         // still can access this outerVariable
+    }
+    
+    return innerFunction;
+}
+
+const myClosure = outerFunction();
+myClosure();        // Output: "I am outside!"
+
+
+
+// We can use closures in data encapsulation as explained in below example
+
+function createCounter() {
+  let count = 0;
+
+  return {
+    increment: () => ++count,
+    getCount: () => count
+  };
+}
+
+const counter = createCounter();
+console.log(counter.increment());       // 1
+console.log(counter.getCount());        // 1
+
+
+
+// Closures are used to create a Function Factories(Creating customized functions) for a specific behaviour
+
+function makeMultiplier(factor) {
+    return function(num) {
+        return num*factor;
+    };
+}
+
+const double = makeMultiplier(2);
+const triple = makeMultiplier(3);
+
+console.log(double(5))      // Here makes double of given number 5;
+console.log(triple(5))      // Here makes triple of given number 5;  
+
+
+
+// Closures capture the correct variable state when using timeouts, intervals, or event handlers.
+
+for (let i = 1; i <= 3; i++) {
+  setTimeout(function () {
+    console.log(`After ${i} second(s): ${i}`);
+  }, i * 1000);
+}
+
+
+
+// Closures allow functions to "remember" results to avoid repeated computation (Memoization/Caching).
